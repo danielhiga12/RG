76a3ab097ec543797bc02d64fdf02b4b1889e010
danielhiga12 <danielhiga121@gmail.com>
2026-01-07 05:40:52 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("🌐 Site policial online na porta " + PORT);
+});
 
